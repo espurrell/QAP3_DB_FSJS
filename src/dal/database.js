19192985172f91ea00900db0360a5b89198241ea
espurrell <150044207+espurrell@users.noreply.github.com>
2024-07-15 22:36:35 -0230

    Phase 9 - update database.js and api.js

--- src/dal/database.js
+++ src/dal/database.js
@@ -13,6 +13,30 @@ const getAllProducts = async () => {
     return res.rows;
 };
 
+const createProduct = async (name, description, price) => {
+    await pool.query('INSERT INTO products (name, description, price) VALUES ($1, $2, $3)', [name, description, price]);
+};
+
+const updateProduct = async (id, name, description, price) => {
+    await pool.query('UPDATE products SET name = $1, description = $2, price = $3 WHERE id = $4', [name, description, price, id]);
+};
+
+const updateProductPrice = async (id, price) => {
+    await pool.query('UPDATE products SET price = $1 WHERE id = $2', [price, id]);
+};
+
+const deleteProduct = async (id) => {
+    await pool.query('DELETE FROM products WHERE id = $1', [id]);
+};
+
+module.exports = {
+    getAllProducts,
+    createProduct,
+    updateProduct,
+    updateProductPrice,
+    deleteProduct,
+};
+
 module.exports = {
     getAllProducts,
 };
